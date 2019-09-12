08dc4d4623ba94e87553fa9a0c5e1a1a80ec0bc5
Randy Taylor <randyisbusy@gmail.com>
2019-09-12 19:50:26 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,15 +7,23 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      begin
+        raise CustomErr
+      rescue CustomErr => error
+        puts error.message
+      end
+    else
     person.partner = self
   end
-
+end
+  class CustomErr < StandardError
+    def message
+    "FUCK YES Im gonna be a pro"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
